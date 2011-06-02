bc52a4a0a6ad01d25cf3edc7d46cb679a9fe005d
Riceball LEE <snowyu.lee@gmail.com>
2011-06-02 15:50:26 +0800

    *[bug] project role settings controller can not work

--- app/controllers/project_role_settings_controller.rb
+++ app/controllers/project_role_settings_controller.rb
@@ -21,9 +21,51 @@ class ProjectRoleSettingsController < RolesController
   layout 'base'
  
   before_filter :find_project, :authorize
+  before_filter :appy_role_project, :only => [:new]
 
   verify :method => :post, :only => [ :destroy, :move ],
          :redirect_to => { :action => :index }
 
+  def appy_role_project
+    params[:role] = {:permissions => Role.non_member.permissions, :project_id => @project.id} if params[:role].blank? and not request.post?
+  end
+
+  def new
+    super
+    @roles = Role.find :all,:conditions=>{:project_id=>@project.id}, :order => 'builtin, position'
+ 
+    return
+    # Prefills the form with 'Non member' role permissions
+    @role = Role.new(params[:role] || {:permissions => Role.non_member.permissions})
+    @role.project=@project
+    if request.post? && @role.save
+      # workflow copy
+      if !params[:copy_workflow_from].blank? && (copy_from = Role.find_by_id(params[:copy_workflow_from]))
+        @role.workflows.copy(copy_from)
+      end
+      flash[:notice] = l(:notice_successful_create)
+      redirect_to :controller=>'projects', :action => 'settings',:id=>params[:id],:tab=>'project_role'
+    end
+    @permissions = @role.setable_permissions
+    @roles = Role.find :all,:conditions=>{:project_id=>@project.id}, :order => 'builtin, position'
+  end
+
+  def edit
+    @role = Role.find(params[:role_id])
+    if request.post? and @role.update_attributes(params[:role])
+      flash[:notice] = l(:notice_successful_update)
+      redirect_to :controller=>'projects', :action => 'settings',:id=>params[:id],:tab=>'project_role'
+    end
+    @permissions = @role.setable_permissions
+  end
+
+  def destroy
+    @role = Role.find(params[:role_id])
+    @role.destroy
+    redirect_to :controller=>'projects', :action => 'settings',:id=>params[:id],:tab=>'project_role'
+  rescue
+    flash[:error] =  l(:error_can_not_remove_role)
+    redirect_to :controller=>'projects', :action => 'settings',:id=>params[:id],:tab=>'project_role'
+  end 
   #  @role.project=@project
 end
