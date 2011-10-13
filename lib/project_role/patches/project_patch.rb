7717a5145f137dece15b73daa7183b98512421e4
Riceball LEE <snowyu.lee@gmail.com>
2011-10-13 08:38:40 +0800

    * [bug] need delete project's roles after the project deleted.

--- lib/project_role/patches/project_patch.rb
+++ lib/project_role/patches/project_patch.rb
@@ -4,15 +4,16 @@ module ProjectRole
       def self.included base
         base.class_eval do
           unloadable
-          has_many :roles, :order => :position
+          has_many :roles, :dependent => :destroy, :order => :position
           after_create   :clone_roles
-
+          
           public
 
           private
           def clone_roles()
             Role.clone_role_to(self)
           end
+
         end
       end
     end
--- lib/project_role/patches/roles_controller_patch.rb
+++ lib/project_role/patches/roles_controller_patch.rb
@@ -7,7 +7,7 @@ module ProjectRole
           around_filter :apply_scope
 
           def apply_scope
-            @project = find_project_by_project_id #if params[:project_id] and params[:action] != 'new' #just workaround
+            @project = find_project_by_project_id if params[:project_id] and params[:action] != 'new' #just workaround
             Role.send(:with_scope, :find => {:conditions => {:project_id => (@project.nil? or params[:action] == 'new') ? nil : @project.id}}) do
               yield
             end
