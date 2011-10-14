46cfeeb138ddd6f84574f2e82e597cdf1d5481d5
stcatz <stcatz@stcatz-VirtualBox.(none)>
2011-10-14 11:52:55 +0800

    *[bug] Addtional record are added when adding project->role. done #327

--- lib/project_role/patches/role_patch.rb
+++ lib/project_role/patches/role_patch.rb
@@ -11,7 +11,7 @@ module ProjectRole
           unloadable
           belongs_to :project
           acts_as_list :scope => :project if  (Role.table_exists? and Role.column_names.include?('project_id'))
-
+   
           def name
             if project
               super# + '(' + project.name + ')'
@@ -42,6 +42,34 @@ module ProjectRole
             match = Role.find_by_name_and_project_id(name, project_id)
             match.nil? or match == self
           end
+          
+          def self.non_member
+            non_member_role=with_exclusive_scope(:find => {:conditions => {:builtin => Role::BUILTIN_NON_MEMBER}}) do
+              find(:first)
+            end
+            p non_member_role
+            if non_member_role.nil?
+              non_member_role = create(:name => 'Non member', :position => 0) do |role|
+                role.builtin = BUILTIN_NON_MEMBER
+              end
+              raise 'Unable to create the non-member role.' if non_member_role.new_record?
+            end
+            non_member_role
+          end
+          
+          def self.anonymous
+            anonymous_role = with_exclusive_scope (:find => {:conditions => {:builtin => Role::BUILTIN_ANONYMOUS}}) do
+              find(:first)
+            end
+            if anonymous_role.nil?
+              anonymous_role = create(:name => 'Anonymous', :position => 0) do |role|
+                role.builtin = BUILTIN_ANONYMOUS
+              end
+              raise 'Unable to create the anonymous role.' if anonymous_role.new_record?
+            end
+            anonymous_role
+          end
+          
         end
       end
 
@@ -57,7 +85,8 @@ module ProjectRole
 
         def find_role_by_project(project_id)
           find(:all, :conditions => {:project_id => project_id}, :order => 'position')
-        end
+        end    
+        
       end
     end
   end
